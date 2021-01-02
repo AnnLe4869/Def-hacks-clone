3ffc1363db10affe9431b73102b1510733800541
Anh Le <kunquan2345@gmail.com>
2021-01-02 04:27:35 -0800

    Style the Home page

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import ReactDOM from 'react-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import ReactDOM from "react-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-import App from './App';
-import ContextWrapper from './context/ContextWrapper';
+import App from "./App";
+import ContextWrapper from "./context/ContextWrapper";
 
 // firebase initialize
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyCBATVLmPyP29uMiXFT_XZsU2CJ43CRFw4',
-  authDomain: 'def-hacks-clone.firebaseapp.com',
-  projectId: 'def-hacks-clone',
-  storageBucket: 'def-hacks-clone.appspot.com',
-  messagingSenderId: '677286999737',
-  appId: '1:677286999737:web:97cb30419f3599e1bf8d68',
-  measurementId: 'G-8FM4D2KR6Z',
+  apiKey: "AIzaSyCBATVLmPyP29uMiXFT_XZsU2CJ43CRFw4",
+  authDomain: "def-hacks-clone.firebaseapp.com",
+  projectId: "def-hacks-clone",
+  storageBucket: "def-hacks-clone.appspot.com",
+  messagingSenderId: "677286999737",
+  appId: "1:677286999737:web:97cb30419f3599e1bf8d68",
+  measurementId: "G-8FM4D2KR6Z",
 });
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline />
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,81 +1,182 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './home.css';
+import { Container, Typography, Button } from "@material-ui/core";
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+import { green, yellow } from "@material-ui/core/colors";
+import clsx from "clsx";
 
-import Footer from '../../common/footer/Footer';
-import Header from '../../common/header/AppBarHeader';
+import Footer from "../../common/footer/Footer";
+import Header from "../../common/header/AppBarHeader";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    welcomeContainer: {
+      marginTop: theme.spacing(5),
+      marginBottom: theme.spacing(5),
+      display: "flex",
+      justifyContent: "center",
+      [theme.breakpoints.up("md")]: {
+        backgroundImage:
+          "url(/assets/guy_left_min.svg) , url(/assets/lady_right_min.svg) ",
+        backgroundRepeat: "no-repeat",
+        // Position one to the left edge, one to the right edge
+        backgroundPosition: "left, right",
+        // Scale width according to image height
+        backgroundSize: "auto 100%",
+      },
+    },
+    welcomeMessageContainer: {
+      [theme.breakpoints.up("md")]: {
+        maxWidth: "50%",
+      },
+    },
+    welcomeTitle: {
+      fontFamily: "IBM Plex Mono, monospace",
+      fontWeight: 700,
+      textShadow: "3px 3px #b6cdb8",
+      marginBottom: theme.spacing(3),
+    },
+    welcomePassage: {
+      fontFamily: "Karla, sans-serif",
+      "& mark": {
+        backgroundColor: "#fcf8e3",
+        padding: theme.spacing(1),
+      },
+    },
+    getStartButton: {
+      // This is for layout, center position
+      display: "block",
+      margin: "auto",
+      marginTop: theme.spacing(3),
+      // For other styling
+      paddingLeft: theme.spacing(3),
+      paddingRight: theme.spacing(3),
+      backgroundColor: green[600],
+      fontWeight: 700,
+      color: "white",
+      boxShadow: "-5px 5px #c5cfd6",
+      borderRadius: 0,
+      "&:hover": {
+        backgroundColor: green[300],
+        boxShadow: "none",
+      },
+
+      [theme.breakpoints.up("md")]: {
+        marginTop: "15vh",
+      },
+    },
+
+    questionPromptContainer: {
+      maxWidth: "40%",
+      backgroundColor: green[100],
+      boxShadow: "2rem -1.5rem #6298ea59",
+      marginTop: "20vh",
+    },
+    questionPromptText: {
+      padding: theme.spacing(2),
+    },
+
+    answerContainer: {
+      // Center the content inside it
+      margin: "auto",
+      // Normal styling
+      marginTop: "20vh",
+      maxWidth: "90%",
+      backgroundColor: yellow[100],
+      boxShadow: "2rem -1.5rem #dbdae199",
+    },
+    answerText: {
+      padding: theme.spacing(2),
+      paddingRight: theme.spacing(3),
+    },
+
+    excitePrompt: {
+      maxWidth: "60%",
+      marginTop: "20vh",
+      backgroundColor: green[100],
+    },
+
+    getStartBottomButton: {
+      fontSize: "3.5rem",
+      boxShadow: "-1rem 1rem #ffb00047",
+    },
+  })
+);
 
 function Home() {
+  const classes = useStyles();
+  const history = useHistory();
+
   return (
     <>
       {/* Header here */}
       <Header />
-      <div className="Home">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-3 d-none d-md-flex">
-              <br />
-              <img
-                src="/assets/guy_left_min.svg"
-                alt="male with a desktop"
-                className="personWithComputer"
-                draggable="false"
-              />
-            </div>
-            <div className="col-12 col-md-6">
-              <h1 className="title"> Def Hacks Learn</h1>
-              <br />
-              <p className="greetingPart">
-                Welcome, <mark>Def-Hackers!</mark> Take your future into your
-                own hands and dive into your first computer science course
-                today, <em>for free!</em>
-              </p>
-              <Link
-                to="/signup"
-                style={{ color: 'inherit', textDecoration: 'inherit' }}
-              >
-                <button className="getStartedButton" type="button">
-                  Get Started
-                </button>
-              </Link>
-            </div>
-            <div className="col-3 d-none d-md-flex">
-              <img
-                src="/assets/lady_right_min.svg"
-                alt="female with a laptop"
-                className="personWithComputer"
-                draggable="false"
-              />
-            </div>
+      <div>
+        {/* Welcome message */}
+        <Container className={classes.welcomeContainer}>
+          <div className={classes.welcomeMessageContainer}>
+            <Typography
+              variant="h2"
+              align="center"
+              className={classes.welcomeTitle}
+            >
+              Def Hacks Learn
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              className={classes.welcomePassage}
+            >
+              Welcome, <mark>Def-Hackers!</mark> Take your future into your own
+              hands and dive into your first computer science course today,{" "}
+              <em>for free!</em>
+            </Typography>
+
+            <Button
+              className={classes.getStartButton}
+              onClick={() => history.push("/learn")}
+            >
+              Start coding
+            </Button>
           </div>
-        </div>
-        <br />
-        <div className="mid">
-          <div className="homeQuestion col-5">
-            {' '}
-            <h2 className="hq-txt">Why Computer Science?</h2>{' '}
-          </div>
-          <div className="d-flex justify-content-end m-5 homeAnswer">
-            <p className="">
-              Technology is seeping into just about every line of work. It’s
-              become necessary for everyone to learn about computers, an
-              integral part of our lives. No matter what you do, computer
-              science will be relevant!
-            </p>
-          </div>
-        </div>
-        <div className="end">
-          <p className="col-8 col-md-7 hook">So what are you waiting for?</p>
-          <Link
-            to="/signup"
-            style={{ color: 'inherit', textDecoration: 'inherit' }}
-          >
-            <button className="col-5 registerNowButton" type="button">
-              <span className="regBtnTxt">Register Now!</span>
-            </button>
-          </Link>
-        </div>
+        </Container>
       </div>
+      {/* Question prompt: why start coding */}
+      <div className={classes.questionPromptContainer}>
+        <Typography
+          align="center"
+          variant="h3"
+          className={classes.questionPromptText}
+        >
+          Why Computer Science?
+        </Typography>
+      </div>
+      {/* Answer: because ... */}
+      <div className={classes.answerContainer}>
+        <Typography align="right" variant="h5" className={classes.answerText}>
+          Technology is seeping into just about every line of work. It’s become
+          necessary for everyone to learn about computers, an integral part of
+          our lives. No matter what you do, computer science will be relevant!
+        </Typography>
+      </div>
+
+      {/* Start prompt */}
+      <div className={classes.excitePrompt}>
+        <Typography
+          align="center"
+          variant="h2"
+          className={classes.questionPromptText}
+        >
+          So what are you waiting for?
+        </Typography>
+      </div>
+      {/* Get start */}
+      <Button
+        className={clsx(classes.getStartButton, classes.getStartBottomButton)}
+        onClick={() => history.push("/learn")}
+      >
+        Get start
+      </Button>
       {/* Footer here */}
       <Footer />
     </>
